e2e465505a85c0ca5a35c1f8bc9d6af8586b38bb
nshutijonathan <nshutijonathan130@gmail.com>
2021-05-04 08:41:47 +0200

    added-blurryLoading

--- hiddenSearchWidget/script.js
+++ hiddenSearchWidget/script.js
@@ -1,6 +1,7 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
+const body = document.querySelector(".body1");
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
   input.focus();
